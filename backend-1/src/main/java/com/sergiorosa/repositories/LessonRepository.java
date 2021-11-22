ff9f3fe9e3eff1a2aef8f5024f315ee7dcea2a20
Sergio Rosa <scarlosjazz@gmail.com>
2021-11-22 12:31:22 -0500

    add: Oauth security, login

--- backend-1/src/main/java/com/sergiorosa/repositories/LessonRepository.java
+++ backend-1/src/main/java/com/sergiorosa/repositories/LessonRepository.java
@@ -2,6 +2,8 @@ package com.sergiorosa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.sergiorosa.entities.Lesson;
+
 public interface LessonRepository extends JpaRepository<Lesson, Long>{
 
 }
